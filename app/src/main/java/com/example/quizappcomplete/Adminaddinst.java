9ca93925b81a35ebada9491e72c72075cd0a3873
rajansurani <43516252+rajansurani@users.noreply.github.com>
2020-06-20 16:58:59 +0530

    Organized Admin Dashboard files.

--- app/src/main/java/com/example/quizappcomplete/Adminaddinst.java
+++ app/src/main/java/com/example/quizappcomplete/Adminaddinst.java
@@ -1,4 +1,4 @@
-package com.example.quizappcomplete.Model;
+package com.example.quizappcomplete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.quizappcomplete.Model.instituteadd;
 import com.example.quizappcomplete.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
